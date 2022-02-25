d1b11a01e0d900e2bdef40d26d715ce671b33de5
Ela Jahoda <3la.jah0da@gmail.com>
2022-02-25 23:36:44 +0100

    zmena obr huntera klavesou

--- main.js
+++ main.js
@@ -7,3 +7,29 @@ if (!( panacekX + panacekSirka < minceX || minceX + minceSirka < panacekX || pan
 
 
 // sem začni psát svůj program
+
+//startPozice
+	let panacek = document.getElementById('panacek');
+	let panacekX = panacek.style.left = '150px';	
+	let panacekY = panacek.style.top = '150px';
+
+function pohybPanacka(event) {
+	let klavesa = event.keyCode;
+	
+	if (klavesa === 39) {
+		panacekX = (parseFloat(panacekX) + 10) + 'px';
+		panacekY = panacekY;
+		panacek.src = "obrazky/panacek-vpravo.png";
+		
+		console.log(panacekX);
+	}
+	if (klavesa === 37) {
+		panacek.src = "obrazky/panacek-vlevo.png";
+	}
+	if (klavesa === 38) {
+		panacek.src = "obrazky/panacek-nahoru.png";
+	}
+	if (klavesa === 40) {
+		panacek.src = "obrazky/panacek.png";
+	}
+}
